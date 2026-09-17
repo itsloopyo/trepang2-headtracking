8e560f4fd043e4c0a8282b95d0414a27bce34cd2
Loop <252084443+itsloopyo@users.noreply.github.com>
2026-09-18 00:25:22 +0100

    Release v0.1.0

--- scripts/install.cmd
+++ scripts/install.cmd
@@ -22,7 +22,7 @@ set "GAME_ID=trepang2"
 set "MOD_DISPLAY_NAME=Trepang2 Head Tracking"
 set "MOD_DLLS=Trepang2HeadTracking.asi"
 set "MOD_INTERNAL_NAME=Trepang2HeadTracking"
-set "MOD_VERSION=0.0.0"
+set "MOD_VERSION=0.1.0"
 set "STATE_FILE=.headtracking-state.json"
 set "FRAMEWORK_TYPE=ASILoader"
 :: Filename the ASI loader DLL is renamed to: the import the game exe already
